3e586235df4f271d09733f24ec994bcc08c9b2cf
1marcghannam <marc@linkpool.io>
2023-01-17 22:57:26 -0500

    feat: Add comment for getExpiringDomains function

--- packages/snap/src/const.ts
+++ packages/snap/src/const.ts
@@ -1,5 +1,17 @@
+/**
+ * ONE_DAY_IN_MILLISECONDS is a constant that holds the number of milliseconds in one day.
+ */
+
 export const ONE_DAY_IN_MILLISECONDS = 86400000;
 
+/**
+ * ONE_WEEK_IN_MILLISECONDS is a constant that holds the number of milliseconds in one week.
+ */
+
 export const ONE_WEEK_IN_MILLISECONDS = ONE_DAY_IN_MILLISECONDS * 7;
+
+/**
+ * MILLISECONDS_MULTIPLIER is a constant that holds the value used to convert seconds to milliseconds.
+ */
 
 export const MILLISECONDS_MULTIPLIER = 1000;
--- packages/snap/src/utils/getExpiringDomains.ts
+++ packages/snap/src/utils/getExpiringDomains.ts
@@ -2,6 +2,14 @@ import { PersistedData } from '../types';
 
 import { ONE_WEEK_IN_MILLISECONDS } from '../const';
 
+/**
+ * getExpiringDomains is a utility function that receives the persisted data of the domains and returns an array of expiring domains.
+ * It will filter the domains that are going to expire within the next week and the expiration date is greater than the current date.
+ *
+ * @param persistedData - The data that holds the ENS domains and their expiration dates
+ * @returns {string[]} An array of expiring domains
+ */
+
 export const getExpiringDomains = async (
   persistedData: PersistedData,
 ): Promise<string[]> => {
